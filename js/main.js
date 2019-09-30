f94a6c5f92aa1e0d7e55fa350dfc704467e15932
Daniil <set2901@mail.ru>
2019-09-30 22:15:33 +0300

    Выполнил валидацию поля ввода хэштегов и привязку индикации глубины эффекта к соответствующему пину

--- js/main.js
+++ js/main.js
@@ -1,16 +1,9 @@
 'use strict';
 
-var getRandomNumber = function (maxNumber) {
-  return Math.floor(Math.random() * maxNumber);
-};
-
-var getRandomArbitrary = function (min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-};
-
-var photosCount = 25;
-var minLikes = 15;
-var maxLikes = 200;
+var ESC_KEYCODE = 27;
+var PHOTOS_COUNT = 25;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
 var COMMENTS_TEXT = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -30,23 +23,94 @@ var userNames = [
 ];
 
 var photosData = [];
-// Заполняем массив photosData
-for (var i = 0; i < photosCount; i++) {
-  var userAvatarNumber = getRandomArbitrary(1, 7);
+var photosGalleryFragment = document.createDocumentFragment();
+var photosGallery = document.querySelector('.pictures');
+var imgUploadForm = document.querySelector('.img-upload__form');
+var uploadFileInput = imgUploadForm.querySelector('.img-upload__input');
+var imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
+var imgUploadCancel = imgUploadForm.querySelector('.img-upload__cancel');
+var effectLevelPin = imgUploadForm.querySelector('.effect-level__pin');
+var effectsItems = imgUploadForm.querySelectorAll('.effects__item');
+var effectLevelDepth = imgUploadForm.querySelector('.effect-level__depth');
+var textHashtags = imgUploadForm.querySelector('.text__hashtags');
+var minHashtagLength = 3;
+var maxHashtagLength = 50;
+var minSymbolsMessage = 'Минимальное количество символов - ' + minHashtagLength;
+var maxSymbolsMessage = 'Максимальное количество символов - ' + maxHashtagLength;
 
-  photosData.push({
-    url: 'photos/' + getRandomArbitrary(1, photosCount + 1) + '.jpg',
-    description: '',
-    likes: getRandomArbitrary(minLikes, maxLikes + 1),
-    comments: []
-  });
+var getRandomNumber = function (maxNumber) {
+  return Math.floor(Math.random() * maxNumber);
+};
 
-  photosData[i].comments.push({
-    avatar: 'img/avatar-' + userAvatarNumber + '.svg',
-    message: COMMENTS_TEXT[getRandomNumber(COMMENTS_TEXT.length)],
-    name: userNames[getRandomNumber(userNames.length)]
-  });
-}
+var getRandomArbitrary = function (min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+var getHashtagsList = function () {
+  var textHashtagsValue = textHashtags.value;
+  var hashtagsList = textHashtagsValue.split([' ']);
+
+  return hashtagsList;
+};
+
+var onTextHashtagsInput = function () {
+  if (textHashtags.getAttribute('minlength') === null) {
+    textHashtags.setAttribute('minlength', String(minHashtagLength));
+  }
+  if (textHashtags.getAttribute('maxlength') === null) {
+    textHashtags.setAttribute('maxlength', String(maxHashtagLength));
+  }
+
+  for (var i = 0; i < getHashtagsList().length; i++) {
+    if (getHashtagsList()[i].length < minHashtagLength) {
+      textHashtags.setCustomValidity('Слишком короткий хэштег');
+    } else {
+      textHashtags.setCustomValidity('');
+    }
+  }
+};
+
+var onTextHashtagsInvalid = function () {
+  if (textHashtags.validity.tooShort) {
+    textHashtags.setCustomValidity(minSymbolsMessage);
+  } else if (textHashtags.validity.tooLong) {
+    textHashtags.setCustomValidity(maxSymbolsMessage);
+  } else if (textHashtags.validity.customError) {
+    textHashtags.setCustomValidity('Слишком короткий хэштег');
+  } else {
+    textHashtags.setCustomValidity('');
+  }
+};
+
+var associateEffectPinWithEffectDepth = function () {
+  var effectLevelPinPosition = window.getComputedStyle(effectLevelPin).left;
+
+  effectLevelDepth.style.width = effectLevelPinPosition;
+};
+
+var onEffectsItemsMouseup = function () {
+  effectLevelDepth.style.width = 0;
+  effectLevelPin.style.left = 0;
+};
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  imgUploadOverlay.classList.remove('hidden');
+
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  imgUploadOverlay.classList.add('hidden');
+  uploadFileInput.value = null;
+
+  document.removeEventListener('keydown', onPopupEscPress);
+};
 
 var renderPicture = function (photoIndex, parrent) {
   var pictureTemplate = document.querySelector('#picture')
@@ -63,12 +127,42 @@ var renderPicture = function (photoIndex, parrent) {
   return picture;
 };
 
-var photosGalleryFragment = document.createDocumentFragment();
-//  Добавляем галерею фотографий в documentFragment
-for (var currentPhoto = 0; currentPhoto < photosCount; currentPhoto++) {
-  renderPicture(currentPhoto, photosGalleryFragment);
+// Заполняем массив photosData
+for (var photosDataI = 0; photosDataI < PHOTOS_COUNT; photosDataI++) {
+  var userAvatarNumber = getRandomArbitrary(1, 7);
+
+  photosData.push({
+    url: 'photos/' + getRandomArbitrary(1, PHOTOS_COUNT + 1) + '.jpg',
+    description: '',
+    likes: getRandomArbitrary(MIN_LIKES, MAX_LIKES + 1),
+    comments: []
+  });
+
+  photosData[photosDataI].comments.push({
+    avatar: 'img/avatar-' + userAvatarNumber + '.svg',
+    message: COMMENTS_TEXT[getRandomNumber(COMMENTS_TEXT.length)],
+    name: userNames[getRandomNumber(userNames.length)]
+  });
 }
 
-var photosGallery = document.querySelector('.pictures');
+//  Добавляем галерею фотографий в photosGalleryFragment
+for (var photoI = 0; photoI < PHOTOS_COUNT; photoI++) {
+  renderPicture(photoI, photosGalleryFragment);
+}
 //  Отрисовываем галерею фотографий
 photosGallery.appendChild(photosGalleryFragment);
+//  Показываем форму редактирования изображения при изменении значения #upload-file
+uploadFileInput.addEventListener('change', openPopup);
+//  Закрываем форму редактирования изображения при клике на крестик
+imgUploadCancel.addEventListener('click', closePopup);
+//  Изменяем ширину индикатора глубины эффекта под положение пина
+effectLevelPin.addEventListener('mouseup', associateEffectPinWithEffectDepth);
+//  Сбрасываем глубину наложенного эффекта на 0% при mouseup на превью эффекта
+for (var effectsItemsI = 0; effectsItemsI < effectsItems.length; effectsItemsI++) {
+  effectsItems[effectsItemsI].addEventListener('mouseup', function () {
+    onEffectsItemsMouseup();
+  });
+}
+
+textHashtags.addEventListener('invalid', onTextHashtagsInvalid);
+textHashtags.addEventListener('input', onTextHashtagsInput);
